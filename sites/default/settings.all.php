9cd107302054afd249326bbb3f0698a93fb5154c
Schnitzel <michael@amazeelabs.com>
2015-06-16 19:29:54 +0200

    Update settings.all.php

--- sites/default/settings.all.php
+++ sites/default/settings.all.php
@@ -5,4 +5,12 @@
  * 
  * This file should contain all settings.php configurations that are needed by all environments.
  */
- 
+
+// Uncomment to use Varnish.
+# $conf['cache_backends'][] = 'sites/all/modules/varnish/varnish.cache.inc';
+# $conf['cache_class_cache_page'] = 'VarnishCache';
+
+// Uncomment to use Redis.
+# $conf['cache_backends'][] = 'sites/all/modules/redis/redis.autoload.inc';
+# $conf['cache_default_class'] = 'Redis_Cache';
+# $conf['cache_prefix'] = getenv('AMAZEEIO_SITENAME') . '_';
