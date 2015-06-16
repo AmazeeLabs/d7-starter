41be9484dc969032c717fc403a060c8c5e6b2702
Schnitzel <michael@amazeelabs.com>
2015-06-16 19:23:02 +0200

    Create settings.all.php

---
+++ sites/default/settings.all.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @file
+ * AmazeeIO Drupal 7 all environment configuration file.
+ * 
+ * This file should contain all settings.php configurations that are needed by all environments.
+ */
+ 
