292e4c8165b4a37bf7c4440ea10ed0c01b8d59f6
Alexander Tkachev <alex.leksat@gmail.com>
2015-07-25 15:07:50 +0300

    DEVOPS-280 Added Solr config override settings

--- sites/default/settings.all.php
+++ sites/default/settings.all.php
@@ -14,3 +14,24 @@
 # $conf['cache_backends'][] = 'sites/all/modules/redis/redis.autoload.inc';
 # $conf['cache_default_class'] = 'Redis_Cache';
 # $conf['cache_prefix'] = getenv('AMAZEEIO_SITENAME') . '_';
+
+if(getenv('AMAZEEIO_SOLR_HOST') && getenv('AMAZEEIO_SOLR_PORT')){
+  // Override search API server settings fetched from default configuration.
+  $conf['search_api_override_mode'] = 'load';
+  $conf['search_api_override_servers'] = array(
+    'solr' => array(
+      'name' => 'Amazee.io Solr - Environment:' . getenv('AMAZEEIO_SITE_ENVIRONMENT'),
+      'options' => array(
+        'host' => getenv('AMAZEEIO_SOLR_HOST'),
+        'port' => getenv('AMAZEEIO_SOLR_PORT'),
+        'path' => '/solr/'.getenv('AMAZEEIO_SITENAME').'/',
+        'http_user' => '',
+        'http_pass' => '',
+        'excerpt' => 0,
+        'retrieve_data' => 0,
+        'highlight_data' => 0,
+        'http_method' => 'POST',
+      ),
+    ),
+  );
+}
