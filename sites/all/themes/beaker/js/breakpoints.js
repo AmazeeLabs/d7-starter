3b77465cb220d18d3eab6cd139f92a156081bb86
saschaeggi <sascha@amazeelabs.com>
2015-09-11 15:30:35 +0200

    BEAKER-22 integrated breakpoints

---
+++ sites/all/themes/beaker/js/breakpoints.js
@@ -0,0 +1,25 @@
+(function ($) {
+
+  /**
+  * CSS Breakpoints import
+  * Usage: if (breakpoint.value == 'BREAKPOINT-VALUE')
+  */
+
+  Drupal.behaviors.importBreakpoints = {
+    attach: function(context, settings) {
+      // Get breakpoint
+      var breakpoint = {};
+      breakpoint.refreshValue = function () {
+        this.value = window.getComputedStyle(document.querySelector('html'), ':before').getPropertyValue('content').replace(/\"/g, '');
+      };
+
+      // Refresh breakpoint var on resize
+      $(window).resize(function () {
+        breakpoint.refreshValue();
+
+        console.log(breakpoint.value);
+      }).resize();
+    }
+  };
+
+})(jQuery);
