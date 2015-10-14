868bc5e0f3c196887dc0cdaa1be0e4459a421b85
saschaeggi <sascha@amazeelabs.com>
2015-10-14 14:19:27 +0200

    added Import of CSS Breakpoints into JS

--- sites/all/themes/beaker/js/behaviors.js
+++ sites/all/themes/beaker/js/behaviors.js
@@ -1,45 +1,59 @@
 (function ($) {
-  Drupal.theme.prototype.standardExampleButton = function (path, title) {
-    // Create an anchor element with jQuery.
-    return $('<a href="' + path + '" title="' + title + '">' + title + '</a>');
+  // Drupal.theme.prototype.standardExampleButton = function (path, title) {
+  //   // Create an anchor element with jQuery.
+  //   return $('<a href="' + path + '" title="' + title + '">' + title + '</a>');
+  // };
+
+  // Get CSS Breakpoints
+  Drupal.behaviors.getBreakpointsFromCSS = {
+    attach: function(context, settings) {
+      var breakpoint = {};
+      breakpoint.refreshValue = function () {
+        this.value = window.getComputedStyle(document.querySelector('html'), ':before').getPropertyValue('content').replace(/\"/g, '');
+      };
+
+      $(window).resize(function () {
+        breakpoint.refreshValue();
+      }).resize();
+    }
   };
 
   // Toggle Hamburger Menu on mobile
-  Drupal.behaviors.searchToggleOnMobile = {
-    attach: function(context, settings) {
-      // Mouse devices
-      $('.sf-accordion-toggle').click(function(e){
-        e.preventDefault();
-        if ( $('> a', this).hasClass('sf-expanded') ) {
-          $('#navigation-search').addClass('sf-expanded');
-        } else {
-          $('#navigation-search').removeClass('sf-expanded');
-        }
-      });
-      // Touch devices
-      $('.sf-accordion-toggle').bind('touchstart', function(e) {
-        if ( $('> a', this).hasClass('sf-expanded') ) {
-          $('#navigation-search').addClass('sf-expanded');
-        } else {
-          $('#navigation-search').removeClass('sf-expanded');
-        }
-      });
-
-      $(window).resize(function(){
-        if($(window).width() > 934) {
-          $('#navigation-search').removeClass('sf-expanded');
-        } else {
-          $('.sf-accordion-toggle a').click(function(){
-            if ( $(this).hasClass('sf-expanded') ) {
-              $('#navigation-search').addClass('sf-expanded');
-            } else {
-              $('#navigation-search').removeClass('sf-expanded');
-            }
-          });
-        }
-      });
-    }
-  };
+  // Drupal.behaviors.searchToggleOnMobile = {
+  //   attach: function(context, settings) {
+  //     // Mouse devices
+  //     $('.sf-accordion-toggle').click(function(e){
+  //       e.preventDefault();
+  //       if ( $('> a', this).hasClass('sf-expanded') ) {
+  //         $('#navigation-search').addClass('sf-expanded');
+  //       } else {
+  //         $('#navigation-search').removeClass('sf-expanded');
+  //       }
+  //     });
+  //     // Touch devices
+  //     $('.sf-accordion-toggle').bind('touchstart', function(e) {
+  //       if ( $('> a', this).hasClass('sf-expanded') ) {
+  //         $('#navigation-search').addClass('sf-expanded');
+  //       } else {
+  //         $('#navigation-search').removeClass('sf-expanded');
+  //       }
+  //     });
+  //
+  //     $(window).resize(function(){
+  //       if($(window).width() > 934) {
+  //         $('#navigation-search').removeClass('sf-expanded');
+  //       } else {
+  //         $('.sf-accordion-toggle a').click(function(){
+  //           if ( $(this).hasClass('sf-expanded') ) {
+  //             $('#navigation-search').addClass('sf-expanded');
+  //           } else {
+  //             $('#navigation-search').removeClass('sf-expanded');
+  //           }
+  //         });
+  //       }
+  //     });
+  //   }
+  // };
 
   //Set footer to bottom
   // Drupal.behaviors.setMainHeight = {
@@ -51,60 +65,60 @@
   // };
 
   //Checkbox & Radio button hack
-  Drupal.behaviors.form = {
-  attach: function (context, settings) {
-    // $('label.option', context).prepend('<span></span>');
-    $('.form-type-radio label', context).prepend('<span class="radio"></span>');
-    $('.form-type-radio input:checked', context).parent().find('span.radio').addClass('checked');
-
-    $('.form-type-radio input', context).change(function() {
-      $('body').find('span.radio').removeClass('checked');
-      $('.form-type-radio input:checked', context).parent().find('span.radio').addClass('checked');
-    });
-
-    $('.form-type-checkbox label', context).prepend('<span class="checkbox"></span>');
-    $('.form-type-checkbox input:checked', context).parent().find('span.checkbox').addClass('checked');
-
-    $('.form-type-checkbox input', context).change(function() {
-      $('body').find('span.checkbox').removeClass('checked');
-      $('.form-type-checkbox input:checked', context).parent().find('span.checkbox').addClass('checked');
-    });
-  }
-};
+//   Drupal.behaviors.form = {
+//   attach: function (context, settings) {
+//     // $('label.option', context).prepend('<span></span>');
+//     $('.form-type-radio label', context).prepend('<span class="radio"></span>');
+//     $('.form-type-radio input:checked', context).parent().find('span.radio').addClass('checked');
+//
+//     $('.form-type-radio input', context).change(function() {
+//       $('body').find('span.radio').removeClass('checked');
+//       $('.form-type-radio input:checked', context).parent().find('span.radio').addClass('checked');
+//     });
+//
+//     $('.form-type-checkbox label', context).prepend('<span class="checkbox"></span>');
+//     $('.form-type-checkbox input:checked', context).parent().find('span.checkbox').addClass('checked');
+//
+//     $('.form-type-checkbox input', context).change(function() {
+//       $('body').find('span.checkbox').removeClass('checked');
+//       $('.form-type-checkbox input:checked', context).parent().find('span.checkbox').addClass('checked');
+//     });
+//   }
+// };
 
 /**
 * Re-Initializes Chosen after Ajax request
 */
-Drupal.behaviors.chosenAjaxRequest = {
-  attach: function(context, settings) {
-    if( settings.chosen ) {
-      settings.chosen = settings.chosen || Drupal.settings.chosen;
-      $(settings.chosen.selector).not('.chosen-enable').addClass('chosen-enable');
-      if ( Drupal.behaviors.chosen ) {
-        Drupal.behaviors.chosen.attach(context, settings);
-      }
-    }
-  }
-};
+// Drupal.behaviors.chosenAjaxRequest = {
+//   attach: function(context, settings) {
+//     if( settings.chosen ) {
+//       settings.chosen = settings.chosen || Drupal.settings.chosen;
+//       $(settings.chosen.selector).not('.chosen-enable').addClass('chosen-enable');
+//       if ( Drupal.behaviors.chosen ) {
+//         Drupal.behaviors.chosen.attach(context, settings);
+//       }
+//     }
+//   }
+// };
 
 /**
 * YouTube video size
 */
-  Drupal.behaviors.YouTubeVideoHeight = {
-    attach: function (context, settings) {
-      function resizeIframe() {
-        var video = $('.embedded-video iframe');
-        var height = (video.width() / 1.8);
-        video.width('100%');
-        video.height(height);
-      }
-      resizeIframe();
-
-      $(window).resize(function() {
-        resizeIframe();
-      });
-    }
-  };
+  // Drupal.behaviors.YouTubeVideoHeight = {
+  //   attach: function (context, settings) {
+  //     function resizeIframe() {
+  //       var video = $('.embedded-video iframe');
+  //       var height = (video.width() / 1.8);
+  //       video.width('100%');
+  //       video.height(height);
+  //     }
+  //     resizeIframe();
+  //
+  //     $(window).resize(function() {
+  //       resizeIframe();
+  //     });
+  //   }
+  // };
 
 /**
 * Default form validation.
@@ -155,5 +169,7 @@ Drupal.behaviors.chosenAjaxRequest = {
 //      });
 //    }
 //  };
+
+
 
 })(jQuery);
